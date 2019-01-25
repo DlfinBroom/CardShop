d5cf25b648e3d82cf117a8579039cec08fa7bed2
lampert309 <lampert309@gmail.com>
2019-01-25 10:47:33 -0800

    Added all variables into Customer class

--- CardShop/Models/Customer.cs
+++ CardShop/Models/Customer.cs
@@ -1,9 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace CardShop.Models {
     public class Customer {
+
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        [Required, DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required, EmailAddress]
+        public string Email { get; set; }
+
+        [Required, DataType(DataType.PhoneNumber) ]
+        public int PhoneNum { get; set; }
     }
 }
